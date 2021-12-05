482152c47bfb3308cb53e946cf67d6ae2a6a854a
Aline Viana <alinemaciel018@gmail.com>
2021-12-05 20:36:53 -0300

    feat: create menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import './menu.styles.css';
+const Menu = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
+                           
