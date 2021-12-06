3cb8409188e6a9f10b2e8d6aefd69de5bff6bb0a
Aline Viana <alinemaciel018@gmail.com>
2021-12-05 22:50:51 -0300

    style: improve style

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import './menu.styles.css';
 const Menu = () => {
   return (
+    <nav>
     <ul>
-      <li>
+      <li className="links">
         <Link to="/">Home</Link>
       </li>
     </ul>
+    </nav>
   );
 };
 
 export default Menu;
-                           
+          
