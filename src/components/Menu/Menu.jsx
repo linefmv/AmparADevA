f76ed576a0cbfccbef3d091820539094084b2ce8
Aline Viana <alinemaciel018@gmail.com>
2021-12-08 20:52:44 -0300

    refactor: add other links and edit style

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
-import './menu.styles.css';
+import "./menu.styles.css";
 const Menu = () => {
   return (
     <nav>
-    <ul>
-      <li className="links">
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
+      <ul>
+        <li className="links">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="links">
+          <Link to="/">Inscreva-se</Link>
+        </li>
+        <li className="links">
+          <Link to="/">Contato</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 export default Menu;
-          
