58cfc11e63cdcdb43542bbb51a8c7c274dd63767
Aline Viana <alinemaciel018@gmail.com>
2021-12-09 05:42:30 -0300

    feat: create new component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import Menu from '../../components/Menu/Menu'
 import Header from '../../components/Header/Header'
-
 import './home.styles.css'
+import TextAndImage from '../../components/TextAndImage/TextAndImage'
+
 const Home = () => {
     return (
         <>
         <Menu />
         <Header />
+        <TextAndImage/> 
+        <TextAndImage>
+        <h1>Alo</h1>
+        </TextAndImage>
+
         </>
        )
 }
