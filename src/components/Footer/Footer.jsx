d5d6d197d727871e837a78fe79369c1a6e0feafa
Aline Viana <alinemaciel018@gmail.com>
2021-12-14 09:13:21 -0300

    refactor: improve footer code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './footer.styles.css'
 const Footer = () => {
     return (
         <footer>
-            <p className='footer-phrase'> Feito com o coração  para a Reprograma por <a href="github.com/linefmv">Aline Viana</a></p>
+            <p className='footer-phrase'> Feito com o ♥ para a Reprograma por <a href="github.com/linefmv">Aline Viana</a></p>
         </footer>    
        )
 }
