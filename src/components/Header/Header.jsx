210512786adbaee27b917b1e038eaecbb97ce223
Aline Viana <alinemaciel018@gmail.com>
2021-12-14 09:15:59 -0300

    refactor: improve code design on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,21 @@
-import './header.styles.css'
-import logo from '../../assets/header-page.svg'
+import "./header.styles.css";
+import logo from "../../assets/header-page.svg";
 
 const Header = () => {
-    return (
-        <header>
-            <img src={logo} alt="draw of girl programming" />
-            <h1>Todes sabemos que os inícios os inícios nem sempre são fáceis, mas eles definem a nossa jornada. E se no teu início tivesse uma mentora incrível para te amparar?</h1>
-            <p> Torne isso possível para mulher em início de carreira. Seja uma mentora! </p>
-            <a href="/subscribe">Inscreva-se!</a>
-        </header>    
-       )
-}
-
+  return (
+    <header>
+      <img src={logo} className="header-page" alt="draw of girl programming" />
+      <h1>
+        Todes sabemos que os inícios os inícios nem sempre são fáceis, mas eles
+        definem a nossa jornada. E se no teu início tivesse uma mentora incrível
+        para te amparar?
+      </h1>
+      <p className="phrase-header">
+        Torne isso possível para mulher em início de carreira. Seja uma mentora!
+      </p>
+      <a href="/subscribe">Inscreva-se!</a>
+    </header>
+  );
+};
 
 export default Header;
