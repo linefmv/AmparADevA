fab0389bb4210c49436ad4bad4de836486546e40
Aline Viana <alinemaciel018@gmail.com>
2021-12-14 22:41:49 -0300

    refactor: fix class name

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import Menu from "../../components/Menu/Menu";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import "./home.styles.css";
 import TextAndImage from "../../components/TextAndImage/TextAndImage";
+import "./home.styles.css";
 import chatGirls from "../../assets/chat-girls.svg";
 import about from "../../assets/about.svg";
-import Footer from "../../components/Footer/Footer";
+
 
 const Home = () => {
   return (
@@ -12,19 +13,19 @@ const Home = () => {
       <Menu />
       <Header />
       <TextAndImage
-        class={"about"}
+        sectionContent={"about"}
         title={"Porque o AmparADevA foi criado?"}
         image={about}
         description={
-          "Embora haja várias iniciativas educacionais para inserir mulheres na tecnologia, o número delas ainda é menor no setor. E há inúmeras barreiras para seguir nesse segmento, "
+          "Embora haja várias iniciativas educacionais para inserir mulheres na tecnologia, o número delas ainda é menor no setor. E há inúmeras barreiras para seguir nesse segmento, e o Amparas sabendo disso vem com a ideia de auxiliar na afirmação da mulher na área."
         }
       />
       <TextAndImage
-        class={"intent"}
+        sectionContent={"intent"}
         title={"Sobre o AmparADevA"}
         image={chatGirls}
         description={
-          "Esse projeto tem o intuito de conectar mulher com mais experiência para auxiliar nas dúvidas sobre carreira, portifolio, tecnologias, empresas, exercícios e vagas. Basta você se inscrever e iremos entrar em contato com você! E ele é para todas, para ti que tem um tempinho e quer ajudar e para você que precisa de ajuda!"
+          "Esse projeto tem o intuito de conectar mulher com mais experiência na área da tecnologia para auxiliar nas dúvidas sobre carreira, portifolio, tecnologias, empresas, exercícios e vagas através de conversas. Basta você se inscrever e iremos entrar em contato com você! E ele é para todas, para ti que tem um tempinho e quer ajudar e para você que precisa de ajuda!"
         }
       />
       <Footer/>
