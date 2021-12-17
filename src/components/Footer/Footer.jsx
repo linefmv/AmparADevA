4311812761821e413120bcf168c6241324aae874
Aline Viana <alinemaciel018@gmail.com>
2021-12-17 08:23:06 -0300

    refactor: change path link on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer>
       <p className="footer-phrase">
         Feito com o <span className="heart">♥ </span>para a Reprograma por{" "}
-        <a className="footer-github" href="github.com/linefmv">
+        <a className="footer-github" href="https://github.com/linefmv">
           Aline Viana
         </a>
       </p>
