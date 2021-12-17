4b96e48fd078678d7e2f76b0adb58c627b486705
Aline Viana <alinemaciel018@gmail.com>
2021-12-16 23:01:18 -0300

    style: add media queries

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,35 +5,42 @@ import "./contact.styles.css";
 const Contact = () => {
   return (
     <>
-     <Menu />
-      <section className="contact-section">
+      <Menu />
+      <section id="contact-section">
         <h2 className="contact-title">
           Alguma dúvida? Entra em contato com a gente!
         </h2>
         <h3 className="social-media">Redes sociais</h3>
         <ul>
           <li>
-            <a href="https://github.com/linefmv">GitHub</a>
+            <a className="github" href="https://github.com/linefmv">
+              GitHub
+            </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/viana-aline/">Linkedin</a>
+            <a
+              className="linkedin"
+              href="https://www.linkedin.com/in/viana-aline/"
+            >
+              Linkedin
+            </a>
           </li>
         </ul>
-        <h3 className="AboutMe-Title">Sobre mim</h3>
         <div className="aboutME-Section">
+        <h3 className="AboutMe-Title">Sobre mim</h3>
           <img
             className="ME"
             src="https://avatars.githubusercontent.com/u/77216386?v=4"
             alt="mulher sorrindo"
           />
           <p className="AboutME">
-            Realizei transição de carreira da saúde para tech e tive muita
-            ajuda, com isso tive o intuíto de criar esse projeto para que essa
-            rede de ajuda aumente cada vez mais.
+            Realizei transição de carreira da saúde para tech e contei com o
+            apoio de várias pessoas, com isso tive o intuíto de criar esse
+            projeto para que essa rede de ajuda aumente cada vez mais.
           </p>
         </div>
       </section>
-    <Footer />
+      <Footer />
     </>
   );
 };
